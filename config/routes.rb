308d32957ccf9b192bdea1fbd72dcddbe69f4406
山口義昌 <yamaguchiyoshimasa@yamaguchiyoshimasanoMacBook-Pro.local>
2022-10-02 22:14:19 +0900

    create Admin::UserController and add

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     get 'login', to: 'user_sessions#new'
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
+    resources :users, only: %i[index show edit update destroy]
+    resources :boards, only: %i[index show edit update destroy]
   end
   resources :users, only: %i[new create]
   resources :boards do
